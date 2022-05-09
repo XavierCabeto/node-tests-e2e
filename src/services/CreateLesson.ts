c3815a12ee3e5d7e3f7d8c8150c63f552d0a3286
XavierCabeto <xaviercabeto@gmail.com>
2022-05-09 18:20:59 +0100

    first commit

---
+++ src/services/CreateLesson.ts
@@ -0,0 +1,23 @@
+import { LessonsRepository } from "../repositories/LessonsRepository";
+
+interface CreateLessonRequest {
+  title: string;
+  description?: string;
+}
+
+export class CreateLesson {
+  constructor(
+    private lessonsRepository: LessonsRepository,
+  ) {}
+
+  async execute({ title, description }: CreateLessonRequest) {
+    if (!title) {
+      throw new Error('Title is required')
+    }
+
+    await this.lessonsRepository.create({
+      title, 
+      description
+    })
+  }
+}
